e1b5e82db5dbd0ce6c7e7798adb3b1fd9eab019e
msdeleyto <msd117c@gmail.com>
2026-01-20 18:13:25 +0100

    refactor: extract variables

--- envs/test/main.tf
+++ envs/test/main.tf
@@ -19,42 +19,17 @@ module "core" {
 module "web" {
   source = "../../modules/project/web"
 
-  project_name       = var.project_name
-  vpc_id             = module.core.vpc_id
-  igw_id             = module.core.igw_id
-  vpc_cidr           = var.vpc_cidr
-  availability_zones = var.availability_zones
-  nacl_rules = [
-    {
-      rule_number = 100
-      egress      = false
-      protocol    = "tcp"
-      cidr        = "0.0.0.0/0"
-      from_port   = 80
-      to_port     = 80
-    },
-    {
-      rule_number = 200
-      egress      = false
-      protocol    = "tcp"
-      cidr        = "0.0.0.0/0"
-      from_port   = 443
-      to_port     = 443
-    },
-    {
-      rule_number = 100
-      egress      = true
-      protocol    = "tcp"
-      cidr        = "0.0.0.0/0"
-      from_port   = 1024
-      to_port     = 65535
-    }
-  ]
-  domain_name         = "test.msdeleyto.es"
+  project_name        = var.project_name
+  vpc_id              = module.core.vpc_id
+  igw_id              = module.core.igw_id
+  vpc_cidr            = var.vpc_cidr
+  availability_zones  = var.availability_zones
+  nacl_rules          = var.web_nacl_rules
+  domain_name         = var.domain_name
   ecr_repository_arns = [local.ecr_repository_arns["web"]]
   service_image       = local.service_images["web"]
   aws_region          = var.aws_region
-  instance_type       = "t4g.micro"
+  instance_type       = var.instance_type
   use_spot            = true
 }
 
